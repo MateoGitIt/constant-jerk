b5b866cfd82195501470de96334520b014bc9177
Mateo Patiño Hasbon <mateopatinohasbon@Mateos-MacBook-Air.local>
2024-03-08 19:32:53 -0500

    Initial commit. RK4 simulation ready for testing.

--- inputParams.py
+++ inputParams.py
@@ -1,6 +1,6 @@
 """
 
-This script allows you to pass in the initial conditions and arguments for [main.py]. Change the value of each variable in lines X through Y
+This script allows you to pass in the initial conditions and arguments for rungekutta.py and simulated.py. Change the value of each variable in lines X through Y
 to achieve the desired initial conditions and step size. The [main.py] script expects the data to arrive through this file and in dictionary-format used below.
 If you change any input parameters here, make sure to make [main.py] compatible to these changes.
 
@@ -12,7 +12,17 @@ the Runge-Kutta method.
 tangentialJerk = 1
 factorQ = 1
 g = 9.81
+initialAcceleration = 1
 initialSpeed = 1
+initialHeight = 1
+xmax = 1
 stepSize = 0.001
 
-parameters = {"j_t":  tangentialJerk, "Q": factorQ, "g": g, "v_0": initialSpeed, "h": stepSize}
+parameters = {"j_t":  tangentialJerk, 
+              "Q": factorQ, 
+              "g": g, 
+              "a_0": initialAcceleration, 
+              "v_0": initialSpeed, 
+              "y_0": initialHeight, 
+              "xmax": xmax, 
+              "h": stepSize}
---
+++ rungekutta.py
@@ -0,0 +1,68 @@
+from inputParams import parameters
+from math import pow, sqrt
+import matplotlib.pyplot as plt
+import numpy as np
+
+# unpack inputs from "inputParams.py"
+Jt, Q, g, a0, v0, y0, xmax, h = parameters.values()
+
+# declare array for U_i, Y_i, and x_i values
+U = np.array()
+Y = np.array()
+X = np.array()
+
+# append initial height
+Y.append(y0)
+
+def main():
+    
+    # calculate U0 (which equals y'(x) at t = 0) and append to U array; SHOULD WE INCLUDE Q IN THE DENOMINATOR? PAPER DOESN'T, jerk2mp.py DOES
+    U0 = -1 * (a0/g) / sqrt(1 - pow(a0/g, 2))
+    U.append(U0)
+
+    # calculate x-axis values with step size h
+    X = np.linspace(0, xmax, h) 
+
+    # calculate U and X lists iteratively
+    for i in range(1, len(X)):
+        k1, k2, k3, k4 = rungekutta_kvalues(U[i-1])
+        new_U = U[i-1] + (h/6)*(k1 + (2*k2) + (2*k3) + k4)
+        U.append(new_U)
+
+    """
+    I will assume that the y-values are calculated from y'(x) using y = y0 + y'(x)dx = y0 + y'(x)h  
+    """
+
+    # calculated y(x) from u(x) = y´(x)
+    for i in range(1, len(X)):
+        new_Y = Y[i-1] + (U[i-1] * h)
+        Y.append(new_Y)
+
+    # plot curve of values X and Y
+    plt.plot(X, Y, lw=1, label = "RK4")
+    plt.xlabel("x (m)")
+    plt.ylabel("y (m)")
+    plt.legend()
+    plt.grid("both")
+    plt.show()
+
+# right-hand side of the autonomous differential equation
+def func(u):
+    numerator = -1 * Jt * Q * pow(1 + pow(u, 2), 2) 
+    denominator = g * sqrt(pow(v0, 2) - (2/Q) * g * h * u )
+    return numerator/denominator
+
+
+def rungekutta_kvalues(u):
+
+    # takes in a u value, calls f(u), computes k-values, and returns them
+    k1 = func(u)
+    k2 = func(u + (h * (k1/2)))
+    k3 = func(u + (h * (k2/2)))
+    k4 = func(u + (h * k3))
+
+    return k1, k2, k3, k4
+
+
+if __name__ == "__main__":
+    main()
